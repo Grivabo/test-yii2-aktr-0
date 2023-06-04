faa601a80eb0670d584d8a2ff2a0a70e28427fb0
Ruslan Strelnikov <grivabo@gmail.com>
2023-06-04 13:10:42 +0300

    TASK-1 migrations

---
+++ migrations/m230603_173940_load_cbt_dump.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types = 1);
+
+use yii\db\Migration;
+
+/**
+ * Load sbt dump.
+ */
+class m230603_173940_load_cbt_dump extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp(): void
+    {
+        $sql = file_get_contents(__DIR__ . '/sql/cbt.sql');
+        $this->execute($sql);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown(): void
+    {
+        $this->dropTable('flight_segment');
+        $this->dropTable('trip_service');
+        $this->dropTable('trip');
+    }
+}
